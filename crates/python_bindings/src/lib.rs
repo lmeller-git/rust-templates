76e500d49e6d37a980430835672880ebf1dcdd42
lmeller-git <louis.meller@icloud.com>
2026-08-31 09:38:15 +0200

    Exclude .github workflows; use snake_case for crate name in Rust/Python code

--- crates/python_bindings/src/lib.rs
+++ crates/python_bindings/src/lib.rs
@@ -17,7 +17,7 @@ fn add_internal(left: u64, right: u64) -> u64 {
 }
 
 #[pymodule]
-fn {{crate_name}}(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn {{crate_name | snake_case}}(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add, m)?)
 }
 
--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
-from {{crate_name}} import add
+from {{crate_name | snake_case}} import add
 
 def main():
-    print(f"Adding two values using {{crate_name}}: 21 + 21 = {add(21, 21)}")
+    print(f"Adding two values using {{crate_name | snake_case}}: 21 + 21 = {add(21, 21)}")
 
 if __name__ == "__main__":
     main()
